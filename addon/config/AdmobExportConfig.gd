a6179436c0fc3ff01bf038cf1adcd68928acc2b5
Cengiz <cengiz@puzly.co>
2025-10-24 11:47:20 +0600

    Optimized export configuration

--- addon/config/AdmobExportConfig.gd
+++ addon/config/AdmobExportConfig.gd
@@ -19,15 +19,77 @@ var debug_application_id: String
 var real_application_id: String
 
 
+func get_config_file_path() -> String:
+	return ""
+
+
 func export_config_file_exists() -> bool:
-	return false
+	return FileAccess.file_exists(get_config_file_path())
 
 
 func load_export_config_from_file() -> Error:
+	Admob.log_info("Loading export config from file!")
+
+	var __result = Error.OK
+
+	var __config_file_path = get_config_file_path()
+	var __config_file = ConfigFile.new()
+
+	var __load_result = __config_file.load(__config_file_path)
+	if __load_result == Error.OK:
+		is_real = __config_file.get_value(CONFIG_FILE_SECTION_GENERAL, CONFIG_FILE_KEY_IS_REAL)
+		debug_application_id = __config_file.get_value(CONFIG_FILE_SECTION_DEBUG, CONFIG_FILE_KEY_APP_ID)
+		real_application_id = __config_file.get_value(CONFIG_FILE_SECTION_RELEASE, CONFIG_FILE_KEY_APP_ID)
+
+		if is_real == null or debug_application_id == null or real_application_id == null:
+			__result = Error.ERR_INVALID_DATA
+		else:
+			__result = load_platform_specific_export_config_from_file(__config_file)
+
+		if __result != Error.OK:
+			Admob.log_error("Invalid export config file %s!" % __config_file_path)
+	else:
+		__result = Error.ERR_CANT_OPEN
+		Admob.log_error("Failed to open export config file %s!" % __config_file_path)
+
+	if __result == Error.OK:
+		print_loaded_config()
+
+	return __result
+
+
+func load_platform_specific_export_config_from_file(a_config_file: ConfigFile) -> Error:
 	return Error.OK
 
 
 func load_export_config_from_node() -> Error:
+	Admob.log_info("Loading export config from node!")
+
+	var __result = Error.OK
+
+	var __admob_node: Admob = get_plugin_node(EditorInterface.get_edited_scene_root())
+	if not __admob_node:
+		var main_scene = load(ProjectSettings.get_setting("application/run/main_scene")).instantiate()
+		__admob_node = get_plugin_node(main_scene)
+
+	if __admob_node:
+		is_real = __admob_node.is_real
+		debug_application_id = __admob_node.android_debug_application_id
+		real_application_id = __admob_node.android_real_application_id
+
+		__result = load_platform_specific_export_config_from_node(__admob_node)
+		if __result == Error.OK:
+			print_loaded_config()
+		else:
+			Admob.log_error("Invalid %s node for %s!" % [PLUGIN_NODE_TYPE_NAME, PLUGIN_NAME])
+	else:
+		Admob.log_error("%s failed to find %s node!" % [PLUGIN_NAME, PLUGIN_NODE_TYPE_NAME])
+		__result = Error.ERR_UNCONFIGURED
+
+	return __result
+
+
+func load_platform_specific_export_config_from_node(a_node: Admob) -> Error:
 	return Error.OK
 
 
